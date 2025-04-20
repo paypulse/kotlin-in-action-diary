846dff8278b2f1469761398c1613276e777f3c6c
엄마는개발자 <sourcedev5@gmail.com>
2025-04-20 22:48:01 +0900

    chore : kotlin 기초 commit (1)

---
+++ src/main/kotlin/01-basic-grammer/Basic.kt
@@ -0,0 +1,8 @@
+package `01-basic-grammer`
+
+class Basic {
+    val nane: String = "test 이름 입니다."
+    var isMarried: Boolean = false
+
+
+}
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,7 @@
+import `01-basic-grammer`.Basic
+
 fun main(){
+    val test = Basic();
+    print("check.Test.:"+ test.nane)
 
 }
